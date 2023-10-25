86cbfe7acd5a76f98156d36347a96e5cf72ae347
akostrik <internet124816@gmail.com>
2023-10-25 21:32:57 +0200

    011/ex02 v1 not sure

---
+++ 01/ex02/main.cpp
@@ -0,0 +1,24 @@
+#include <cctype>
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include <list>
+#include <cstddef>
+#include <iostream>
+
+int	main()
+{
+  std::string s          = "HI THIS IS BRAIN";
+  std::string *stringPTR = &s;
+  std::string stringREF  = s;
+
+  std::cout << "The memory address of s              " << &s         << std::endl;
+  std::cout << "The memory address held by stringPTR " << stringPTR  << std::endl;
+  std::cout << "The memory address held by stringREF " << &stringREF  << std::endl;
+  std::cout << "The value of the string variable     " << s          << std::endl;
+  std::cout << "The value pointed to by stringPTR    " << *stringPTR << std::endl;
+  std::cout << "The value pointed to by stringREF    " << stringREF << std::endl;
+
+  return (0);
+}
