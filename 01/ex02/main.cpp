3097777345d3bba6de7cc2e49b0c23e6efbaa869
akostrik <internet124816@gmail.com>
2023-10-25 21:43:47 +0200

    011/ex02 finished

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -11,14 +11,16 @@ int	main()
 {
   std::string s          = "HI THIS IS BRAIN";
   std::string *stringPTR = &s;
-  std::string stringREF  = s;
+  std::string &stringREF = s;
 
-  std::cout << "The memory address of s              " << &s         << std::endl;
-  std::cout << "The memory address held by stringPTR " << stringPTR  << std::endl;
-  std::cout << "The memory address held by stringREF " << &stringREF  << std::endl;
-  std::cout << "The value of the string variable     " << s          << std::endl;
-  std::cout << "The value pointed to by stringPTR    " << *stringPTR << std::endl;
-  std::cout << "The value pointed to by stringREF    " << stringREF << std::endl;
+  std::cout << "address of s                  " << &s           << std::endl;
+  std::cout << "// address &(s[0])            " << &(s[0])      << std::endl;
+  std::cout << "// address (void*)&s[0]       " << (void*)&s[0] << std::endl;
+  std::cout << "address held by stringPTR     " << stringPTR    << std::endl;
+  std::cout << "address held by stringREF     " << &stringREF   << std::endl;
+  std::cout << "value s                       " << s            << std::endl;
+  std::cout << "value pointed to by stringPTR " << *stringPTR   << std::endl;
+  std::cout << "value pointed to by stringREF " << stringREF    << std::endl;
 
   return (0);
 }
