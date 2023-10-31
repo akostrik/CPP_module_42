d0d8c8815cb272e37412e02a4265681d7756434a
ann <internet124816@gmail.com>
2023-10-31 14:51:01 +0100

    .

--- 01/ex03/Weapon.cpp
+++ 01/ex03/Weapon.cpp
@@ -3,6 +3,7 @@
 Weapon::Weapon(std::string type_) : type(type_) {
 };
 
+// The getType() function returns a const reference to the type string !
 std::string Weapon::getType(void) {
     return this->type;
 };
