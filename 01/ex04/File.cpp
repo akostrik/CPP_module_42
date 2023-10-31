d7a95f701cdaadfbb5895486c804583c2bd4622f
ann <internet124816@gmail.com>
2023-10-31 15:45:25 +0100

    00/ex04 works but not final

--- 01/ex04/File.cpp
+++ 01/ex04/File.cpp
@@ -12,44 +12,38 @@ File::File(const char *in_file_) : in_file(in_file_){
 int File::replace(std::string s1, std::string s2) {
   std::ifstream in(this->in_file);
 	std::string   in_file_str = this->in_file;
-  std::ofstream out((in_file_str + ".replace").c_str());
+  std::ofstream out;
   std::string   content = "";
   std::string   line;
 
-  if (!in.is_open() || !out.is_open())
+  if (!in.is_open())
   {
-		std::cout << "file open problem (" << this->in_file << " or " << (in_file_str + ".replace").c_str() << ")" << std::endl;
+		std::cout << "can't open file " << this->in_file << std::endl;
+    return 1;
+  }
+  out.open((in_file_str + ".replace").c_str());
+	if (!out.is_open())
+  {
+		std::cout << "can't open file " << (in_file_str + ".replace").c_str() << std::endl;
     return 1;
   }
 
   while (getline (in, line))
-  {
-		content += line + "\n";
-    std::cout << line << std::endl;
-    out       << line << std::endl;
-  }
+    content += line + "\n";
 	if (content.length() > 0)
     content.resize(content.length() - 1);
 
-  std::cout << "content :\n" << content << std::endl;
-
 	std::size_t pos = 0;
-  for (int i = 0; 1; i++)
+  for (int i = 0; (pos = content.find(s1, i)) != std::string::npos; i++)
 	{
-		pos = content.find(s1, i);
-		if (pos == std::string::npos)
-			break ;
-    std::cout << "pos = " << pos << '\n';
-		content.erase(pos, s1.length());
-		content.insert(pos, s2);
-		i += s2.length();
+		content.erase(pos, s1.length()).insert(pos, s2);
+		i += s2.length() - 1;
 	}
 
   std::cout << "content :\n" << content << std::endl;
+  out       << content << std::endl;
 
   in.close();
   out.close();
-  (void)s1;
-  (void)s2;
 	return 0;
 };
