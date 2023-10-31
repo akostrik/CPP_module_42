8220eb2b6e54f006c14dd95fb61c908bc555c5a3
ann <internet124816@gmail.com>
2023-10-31 10:38:22 +0100

    .

--- 01/ex03/HumanA.hpp
+++ 01/ex03/HumanA.hpp
@@ -1,10 +1,8 @@
 #ifndef HUMANA_HPP
 # define NUMANA_HPP
-
 # include "Weapon.hpp"
 
 class HumanA {
-
 private:
 	std::string name;
 	Weapon&     weaponREF; //
--- 01/ex03/HumanB.hpp
+++ 01/ex03/HumanB.hpp
@@ -1,17 +1,15 @@
 #ifndef HUMANB_HPP
 # define NUMANB_HPP
-
 # include "Weapon.hpp"
 
-class HumanB
-{
+class HumanB {
 private:
 	std::string name;
 	Weapon*     weaponPTR; //
 
 public:
 	            HumanB(std::string name_);
-	void        setWeapon(Weapon& weaponPTR_); //
+	void        setWeapon(Weapon& weaponPTR_); // &
 	void        attack(void);
 };
 
