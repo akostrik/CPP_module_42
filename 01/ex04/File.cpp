6d37b7e6be7bc121addfb80f032439089717f226
ann <internet124816@gmail.com>
2023-10-31 16:34:46 +0100

    01/ex04 ok

--- 01/ex04/File.cpp
+++ 01/ex04/File.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <string> //
 
-// try to pass a file that does not exist, change the permissions, pass it empty, etc.
+// a file that does not exist, change the permissions
 // empty string
-// s1 = a, s2 = aaa
+// s1 = a, s2 = aa
+// s1 = "" or s2 == ""
 
 File::File(const char *in_file_) : in_file(in_file_){
 };
@@ -15,12 +16,33 @@ int File::replace(std::string s1, std::string s2) {
   std::ofstream out;
   std::string   content = "";
   std::string   line;
+	std::size_t   pos;
+
+	if (s1.length() == 0)
+		return 1;
 
   if (!in.is_open())
   {
 		std::cout << "can't open file " << this->in_file << std::endl;
     return 1;
   }
+  while (getline (in, line))
+    content += line + "\n";
+	if (content.length() > 0)
+    content.resize(content.length() - 1);
+  in.close();
+
+  for (int i = 0; 1; )
+	{
+	  std::cout << "*** content :\n" << content << std::endl;
+		pos = content.find(s1, i);
+		if (pos == std::string::npos)
+			break ;
+		content.erase(pos, s1.length()).insert(pos, s2);
+		i = pos + s2.length();
+		std::cout << "pos = " << pos << ", i = " << i << std::endl << std::endl;
+	}
+
   out.open((in_file_str + ".replace").c_str());
 	if (!out.is_open())
   {
@@ -28,32 +50,8 @@ int File::replace(std::string s1, std::string s2) {
 		std::cout << "can't open file " << (in_file_str + ".replace").c_str() << std::endl;
     return 1;
   }
-
-  while (getline (in, line))
-    content += line + "\n";
-	if (content.length() > 0)
-    content.resize(content.length() - 1);
-
-  for (int i = 0; 1; i++)
-	{
-	  std::cout << "*** content :\n" << content << std::endl;
-		std::size_t pos = content.find(s1, i);
-		if (pos ==std::string::npos)
-			break ;
-		if (s1.length() == 1)
-		{
-			content.erase(pos, s1.length()).insert(pos, s2);
-			i = pos + s2.length(); // ?
-		}
-		else
-		{
-			content.erase(pos, s1.length()).insert(pos, s2);
-			i = pos + s2.length() - 1;
-		}
-		std::cout << "pos = " << pos << ", i = " << i << std::endl << std::endl;
-	}
-
-  in.close();
+	out << content;
   out.close();
+
 	return 0;
 };
