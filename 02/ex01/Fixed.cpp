8e4e91d7ce3b43902acec3190a9cd5a697f96df5
ann <internet124816@gmail.com>
2023-11-09 14:57:08 +0100

    02/01 in progress

--- 02/ex01/Fixed.cpp
+++ 02/ex01/Fixed.cpp
@@ -1,14 +1,50 @@
 #include "Fixed.hpp"
 
+///////////////////////////// utils
+int nb_decimal_figures(float const _fpv)
+{
+  float fpv = _fpv;
+  for (int i = 0; i < 8; i++)
+	{
+    if (roundf(fpv * 10) == roundf(fpv) * 10)
+      return (i);
+    fpv = 10 * fpv;
+  }
+	return (8);
+}
+
+int pow(int a, int n) {
+  int res = 1;
+  for(int i = 0; i < n; i++)
+    res *= a;
+  return (res);
+}
+
+/////////////////////////////
 Fixed::Fixed() {
   std::cout << "Default constructor called" << std::endl;
   this->fpv = 0;
+  this->n = 8;
+}
+
+// NEW converts p to the corresponding fpv
+// INT_MAX ?
+Fixed::Fixed(int const _fpv) {
+  std::cout << "Int constructor called" << std::endl;
+  this->fpv = _fpv * 100000000;
+  this->n = 8;
+}
+
+// NEW converts p to the corresponding fpv
+Fixed::Fixed(float const _fpv) {
+  std::cout << "Float constructor called" << std::endl;
+  this->n = nb_decimal_figures(_fpv);
+  this->fpv = roundf(_fpv * pow(10, this->n));
 }
 
 // a new object is created as a copy of the existing object
 Fixed::Fixed(const Fixed &obj) {
   std::cout << "Copy constructor called" << std::endl;
-  // this->setRawBits(obj.getRawBits()); ?
 	*this = obj;
 }
 
@@ -16,7 +52,10 @@ Fixed::Fixed(const Fixed &obj) {
 Fixed& Fixed::operator = (Fixed const &obj) {
   std::cout << "Copy assignment operator called" << std::endl;
   if (this != &obj)
+  {
     this->fpv = obj.getRawBits();
+    this->n = 8;
+  }
   return (*this);
 }
 
@@ -25,10 +64,26 @@ Fixed::~Fixed() {
 };
 
 int Fixed::getRawBits(void) const {
-  std::cout << "getRawBits member function called" << std::endl;
   return this->fpv;
 }
 
 void Fixed::setRawBits(const int _fpv) {
 	this->fpv = _fpv;
+}
+
+// NEW member function (Fixed::), converts the fixed-point value to a floating-point value
+float Fixed::toFloat(void) const {
+  return roundf(this->fpv / pow(10,this->n)) + ((float)(this->fpv % pow(10,this->n))) / pow(10,this->n);
+}
+
+// NEW member function (Fixed::), converts the fixed-point value to an integer value
+int Fixed::toInt(void) const {
+  return roundf(this->fpv /  pow(10,this->n));
+}
+
+// NEW overload of the insertion operator <<
+std::ostream& operator<<(std::ostream& os, Fixed const & f)
+{
+  os << f.toFloat();
+  return os;
 }
--- 02/ex01/Fixed.hpp
+++ 02/ex01/Fixed.hpp
@@ -5,24 +5,26 @@
 # include <iostream>
 # include <fstream>
 # include <map>
+# include <cmath>
 
 class Fixed {
 private:
-  int              fpv;                       // fixed-point number value
-  static int const n = 8;                     // number of fractional bits will always be the integer literal 8
+  int         n;                         // number of fractional bits
+  int         fpv;                       // fixed-point number value
 
 public:
-                   Fixed();
-                   Fixed(int const v);        // converts p to the corresponding fpv, n is initialized to 8 NEW
-                   Fixed(float const v);      // converts p to the corresponding fpv, n is initialized to 8 NEW
-                   Fixed(const Fixed &obj);
-                   Fixed & operator = (const Fixed &obj);
-                   ~Fixed();
-  void             setRawBits(int const raw); // sets the raw value of the fixed-point number
-  int              getRawBits(void) const;    // returns the raw value of the fixed-point value
-  float            toFloat(void) const;       // converts the fixed-point value to a floating-point value   NEW
-  int              toInt(void) const;         // converts the fixed-point value to an integer value         NEW
-  void             f()	// an overload of the insertion (<<) operator, inserts a floating-p representation of the fixed-p number into the output stream object passed as param
+              Fixed();
+              Fixed(int const _fpv);   // NEW converts p to the corresponding fpv, n is initialized to 8
+              Fixed(float const _fpv);// NEW converts p to the corresponding fpv, n is initialized to 8
+              Fixed(const Fixed &obj);
+  Fixed&      operator = (const Fixed &obj);
+              ~Fixed();
+  void        setRawBits(int const raw); // sets the raw value of the fixed-point number
+  int         getRawBits(void) const;    // returns the raw value of the fixed-point value
+  float       toFloat(void) const;       // NEW converts the fixed-point value to a floating-point value
+  int         toInt(void) const;         // NEW converts the fixed-point value to an integer value
 };
+
+std::ostream& operator<<(std::ostream& os, Fixed const & f); // NEW overload of the insertion operator <<, inserts a floating-p representation of the fp number into the output stream object passed as param
 
 #endif
