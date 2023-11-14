06d8feaa16a82234a88232b1b2910f1d86bbcd6e
akostrik <internet124816@gmail.com>
2023-11-14 22:20:39 +0100

    02/ex01 ok

--- 02/ex01/Fixed.cpp
+++ 02/ex01/Fixed.cpp
@@ -1,63 +1,28 @@
 #include "Fixed.hpp"
 
-///////////////////////////// utils
-int nb_decimal_figures(float const _fpv)
-{
-  float fpv = _fpv;
-  for (int i = 0; i < 8; i++)
-	{
-    if (roundf(fpv * 10) == roundf(fpv) * 10)
-      return (i);
-    fpv = 10 * fpv;
-  }
-	return (8);
-}
-
-int pow(int a, int n) {
-  int res = 1;
-  for(int i = 0; i < n; i++)
-    res *= a;
-  return (res);
-}
-
-/////////////////////////////
 Fixed::Fixed() {
   std::cout << "Default constructor called" << std::endl;
   this->raw = 0;
 }
 
-// // NEW converts p to the corresponding fpv
-// Fixed::Fixed(int const _raw) {
-//   std::cout << "Int constructor called" << std::endl;
-//   this->raw = _raw * 100000000;
-// }
-
-// // NEW converts p to the corresponding 
-// // INT_MAX = 2 147 483 647
-// Fixed::Fixed(float const _raw) {
-//   std::cout << "Float constructor called" << std::endl;
-//   this->raw = roundf(_raw * 100000000);
-//   std::cout << "raw = " << _raw << " * 100 000 000 = " << this->raw << std::endl;
-// }
-
-Fixed::Fixed(const int _raw) {
+// NEW converts p to the corresponding fpv
+Fixed::Fixed(const int raw_) {
   std::cout << "Int constructor called" << std::endl;
-  this->raw = _raw << 8;
+  this->raw = raw_ * 256; // raw_ << 8;
 }
 
-Fixed::Fixed(const float _raw) {
+// NEW converts p to the corresponding 
+Fixed::Fixed(const float raw_) {
   std::cout << "Float constructor called"       << std::endl;
-  std::cout << "  _raw            = : "         << _raw << std::endl;
-  std::cout << "  1 << 8          = : "         << (1 << 8) << std::endl;
-  std::cout << "  _raw * (1 << 8) = : "         << _raw * (1 << 8) << std::endl;
-  std::cout << "  roundf(_raw * (1 << 8)) = : " << roundf(_raw * (1 << 8)) << std::endl;
-	this->raw = roundf(_raw * (1 << 8));
+  int raw_int = (int)(raw_);
+  int raw_dec = roundf((raw_ - (int)raw_)*256);
+  this->raw = raw_int * 256 + raw_dec;
 }
 
 // a new object is created as a copy of the existing object
 Fixed::Fixed(const Fixed &obj) {
   std::cout << "Copy constructor called" << std::endl;
-	*this = obj; // ?
+  *this = obj; // ?
 }
 
 // an already initialized object is assigned to a new value from another existing object
@@ -82,12 +47,12 @@ void Fixed::setRawBits(const int _raw) {
 
 // NEW member function (Fixed::), converts the fixed-point value to a floating-point value
 float Fixed::toFloat(void) const {
-  return roundf(this->raw >> 8) + (float)(((this->raw % 256)) >> 8);
+  return roundf(this->raw / 256) + ((this->raw % 256)) / 256.0;
 }
 
 // NEW member function (Fixed::), converts the fixed-point value to an integer value
 int Fixed::toInt(void) const {
-  return roundf(this->raw >> 8);
+  return roundf(this->raw / 256);
 }
 
 // NEW overload of the insertion operator <<
