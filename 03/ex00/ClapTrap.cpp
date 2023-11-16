0bbf34f33275a081e0831886ca827190fa26c761
akostrik <internet124816@gmail.com>
2023-11-16 01:41:05 +0100

    03/ex00 in progress

--- 03/ex00/ClapTrap.cpp
+++ 03/ex00/ClapTrap.cpp
@@ -1,27 +1,59 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap() {
-  std::cout << "Default constructor called" << std::endl;
-  this->raw = 0;
-}
+  std::cout << "Anonymus default constructor called" << std::endl;
+  this->name = "anonymus";
+  this->hitPoints    = 10;
+  this->energyPoints = 10;
+  this->attackDamage = 0;
+};
 
-ClapTrap::ClapTrap() {
-  std::cout << "Int constructor called" << std::endl;
-  this->raw = 0;
-}
+ClapTrap::ClapTrap(std::string name_) {
+  std::cout << name_ << " string constructor called" << std::endl;
+  if(name_ == "")
+    this->name = "anonymus";
+  else
+    this->name = name_;
+  this->hitPoints    = 10;
+  this->energyPoints = 10;
+  this->attackDamage = 0;
+};
 
 // a new object is created as a copy of the existing object
 ClapTrap::ClapTrap(const ClapTrap &obj) {
   *this = obj; // ?
-}
+};
 
 // an already initialized object is assigned to a new value from another existing object
 // copy assignment operator overload
 ClapTrap& ClapTrap::operator = (ClapTrap const &obj) {
-  if (this != &obj)
-    this->raw = obj.getRawBits();
+  if (this != &obj) {
+    this->name         = obj.name;
+    this->hitPoints    = obj.hitPoints;
+    this->energyPoints = obj.energyPoints;
+    this->attackDamage = obj.attackDamage;
+  }
   return (*this);
-}
+};
 
 ClapTrap::~ClapTrap() {
+  std::cout << this->name << " destructor called" << std::endl;
 };
+
+ClapTrap&   operator = (const ClapTrap &obj) {
+
+}
+
+void ClapTrap::attack(const std::string& target) {
+
+};
+
+void ClapTrap::takeDamage(unsigned int amount) {
+
+};
+
+void ClapTrap::beRepaired(unsigned int amount) {
+
+};
+
+// ClapTrap <name> attacks <target>, causing <damage> points of damage!
--- 03/ex00/ClapTrap.hpp
+++ 03/ex00/ClapTrap.hpp
@@ -9,16 +9,20 @@
 
 class ClapTrap {
 private:
-  int              raw;
-  static int const n = 8;
+  std::string name ;
+  int         hitPoints;    // the health
+  int         energyPoints; // can’t do anything if it has no hit points or energy points
+  int         attackDamage;
 
 public: 
-                   ClapTrap();
-                   ClapTrap(std::string name);
-                   ClapTrap(const ClapTrap &obj);
-                   ~ClapTrap();
-  ClapTrap&        operator = (const ClapTrap &obj);
-
+              ClapTrap   ();
+              ClapTrap   (std::string name);
+              ClapTrap   (const ClapTrap &obj);
+              ~ClapTrap  ();
+  ClapTrap&   operator = (const ClapTrap &obj);
+  void        attack     (const std::string& target); // causes its target to lose <attack damage> hit points, costs 1 energyPoint
+  void        takeDamage (unsigned int amount);
+  void        beRepaired (unsigned int amount);       // gets <amount> hit points back, costs 1 energyPoint
 };
 
 #endif
