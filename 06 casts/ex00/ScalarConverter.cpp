7bb208e9023b2ac3bcf3d16c2f327759acf8fafc
ann <internet124816@gmail.com>
2024-01-15 15:03:12 +0100

    06/ex00 finihed

--- 06 casts/ex00/ScalarConverter.cpp
+++ 06 casts/ex00/ScalarConverter.cpp
@@ -1,7 +1,3 @@
-// s.back() != 'f' 
-// s.find( '.' ) == 0
-// s.compare( "nan" ) == 0 
-
 #include "ScalarConverter.hpp"
 
 bool ScalarConverter::fInTheEnd(std::string s) {
@@ -9,56 +5,28 @@ bool ScalarConverter::fInTheEnd(std::string s) {
 
   for (len = 0; s[len] != '\0'; len++) ;
   return (s[len - 1] == 'f');
-} 
+}
 
 bool ScalarConverter::thereIsPoint(std::string s) {
   for (int i = 0; s[i] != '\0'; i++) 
     if (s[i] == '.')
       return true;
   return false;
-} 
-
-int ScalarConverter::strlen(std::string s) {
-  int len;
-
-  for (len = 0; s[len] != '\0'; len++) ;
-  return len;
 }
 
-bool ScalarConverter::isSingleDigitNumber(std::string s) {
-  if (ScalarConverter::strlen(s) == 1 && s[0] >= '0' && s[0] <= '9')
+bool ScalarConverter::isInteger(std::string s) { // can be more than INT_MAX, but still an integer number
+  int i;
+
+  if (!ScalarConverter::thereIsPoint(s))
     return true;
-  if (ScalarConverter::strlen(s) == 2 && s[0] >= '0' && s[0] <= '9'   && s[1] == 'f')
-    return true;
-  if (ScalarConverter::strlen(s) == 2 && (s[0] == '+' || s[0] == '-') && s[1] >= '0' && s[1] <= '9')
-    return true;
-  if (ScalarConverter::strlen(s) == 3 && (s[0] == '+' || s[0] == '-') && s[1] >= '0' && s[1] <= '9' && s[2] == 'f')
-    return true;
-  return false;
-}
-
-bool ScalarConverter::isChar(std::string s) {
-  return (ScalarConverter::strlen(s) == 1 && s[0] >= CHAR_MIN && !(s[0] >= '0' && s[0] <= '9'));
-}
-
-bool ScalarConverter::isDisplayableChar(std::string s) {
-  return (ScalarConverter::strlen(s) == 1 && s[0] >= 32 && s[0] <= 126 && !(s[0] >= '0' && s[0] <= '9'));
-}
-
-// bool ScalarConverter::isDisplayableChar(double d) {
-//   return (d >= 32 && d <= 126);
-// }
-
-// bool ScalarConverter::isNonDisplayableChar(double d) {
-//   return (d >= CHAR_MIN && d <= CHAR_MAX && (d < 32 || d > 126));
-// }
-
-// bool ScalarConverter::isDisplayableChar(char c) {
-//   return (c >= 32 && c <= 126);
-// }
-
-bool ScalarConverter::isNonDisplayableChar(std::string s) {
-  return (ScalarConverter::strlen(s) == 1 && s[0] >= CHAR_MIN && (s[0] < 32 || s[0] > 126));
+  for (i = 0; s[i] != '\0'; i++) ;
+  i--;
+  if (s[i] == 'f')
+    i--;
+  for (     ; s[i] != '.' ; i--)
+    if (s[i] != '0')
+      return false;
+  return true;
 }
 
 void ScalarConverter::convert(std::string s) {
@@ -86,7 +54,7 @@ void ScalarConverter::convert(std::string s) {
     std::cout << "(detected type: special)" << std::endl;
     return ;
   }
-  if (ScalarConverter::isChar(s)) {
+  if (s[1] == '\0' && s[0] >= CHAR_MIN && !(s[0] >= '0' && s[0] <= '9')) { // is char
     char c = s[0];
     if (c >= 32 && c <= 126)
       std::cout << "char:   '" << c << "' (detected type)" << std::endl;
@@ -110,10 +78,14 @@ void ScalarConverter::convert(std::string s) {
   }
   if (d >= INT_MIN && d <= INT_MAX && !ScalarConverter::fInTheEnd(s) && !thereIsPoint(s)) {
     int i = std::atoi(s.c_str());
-    if (static_cast<char>(i) >= 32 && static_cast<char>(i) <= 126)
+    if (d >= 32 && d <= 126 && ScalarConverter::isInteger(s))
       std::cout << "char:   '" << static_cast<char>(i) << "'" << std::endl;
-    else
+    else if (d >= CHAR_MIN && d <= CHAR_MAX && ScalarConverter::isInteger(s))
       std::cout << "char:   Non displayable" << std::endl;
+    else if ((d < CHAR_MIN || d > CHAR_MAX) && ScalarConverter::isInteger(s))
+      std::cout << "char:   out of range [CHAR_MIN; CHAR_MAX]" << std::endl;
+    else
+      std::cout << "char:   conversion doesn't make sens" << std::endl;
     std::cout << "int:    " << i << " (detected type)" << std::endl;
     std::cout << std::fixed << std::setprecision(2) << "float:  " << static_cast<float>(i) << "f" << std::endl;
     std::cout << std::fixed << std::setprecision(2) << "double: " << static_cast<double>(i) << std::endl;
@@ -121,31 +93,39 @@ void ScalarConverter::convert(std::string s) {
   }
   if (d >=-FLT_MAX && d <= FLT_MAX && ScalarConverter::fInTheEnd(s)) {
     float f = std::strtof(s.c_str(), NULL);
-    if (static_cast<char>(f) >= 32 && static_cast<char>(f) <= 126)
+    if (d >= 32 && d <= 126 && ScalarConverter::isInteger(s))
       std::cout << "char:   '" << static_cast<char>(f) << "'" << std::endl;
-    else
+    else if (d >= CHAR_MIN && d <= CHAR_MAX && ScalarConverter::isInteger(s))
       std::cout << "char:   Non displayable" << std::endl;
+    else if ((d < CHAR_MIN || d > CHAR_MAX) && ScalarConverter::isInteger(s))
+      std::cout << "char:   out of range [CHAR_MIN; CHAR_MAX]" << std::endl;
+    else
+      std::cout << "char:   conversion doesn't make sens" << std::endl;
     if (d >= INT_MIN && d <= INT_MAX)
       std::cout << "int:    " << static_cast<int>(f) << std::endl;
     else
-      std::cout << "int:    string-argument out of range [INT_MIN; INT_MAX]" << std::endl;
+      std::cout << "int:    out of range [INT_MIN; INT_MAX]" << std::endl;
     std::cout << std::fixed << std::setprecision(2) << "float:  " << f << "f (detected type)" << std::endl;
     std::cout << std::fixed << std::setprecision(2) << "double: " << static_cast<double>(f) << std::endl;
     return ;
   }
   if (!ScalarConverter::fInTheEnd(s)) {
-    if (static_cast<char>(d) >= 32 && static_cast<char>(d) <= 126)
+    if (d >= 32 && d <= 126 && ScalarConverter::isInteger(s))
       std::cout << "char:   '" << static_cast<char>(d) << "'" << std::endl;
-    else
+    else if (d >= CHAR_MIN && d <= CHAR_MAX && ScalarConverter::isInteger(s))
       std::cout << "char:   Non displayable" << std::endl;
+    else if ((d < CHAR_MIN || d > CHAR_MAX) && ScalarConverter::isInteger(s))
+      std::cout << "char:   out of range [CHAR_MIN; CHAR_MAX]" << std::endl;
+    else
+      std::cout << "char:   conversion doesn't make sens" << std::endl;
     if (d >= INT_MIN && d <= INT_MAX)
       std::cout << "int:    " << static_cast<int>(d) << std::endl;
     else
-      std::cout << "int:    string-argument out of range [INT_MIN; INT_MAX]" << std::endl;
+      std::cout << "int:    out of range [INT_MIN; INT_MAX]" << std::endl;
     if (d >= -FLT_MAX && d <= FLT_MAX)
       std::cout << std::fixed << std::setprecision(2) << "float:  " << static_cast<float>(d) << "f" << std::endl;
     else
-      std::cout << "float:  string-argument out of range [FLT_MIN; FLT_MAX]" << std::endl;
+      std::cout << "float:  out of range [FLT_MIN; FLT_MAX]" << std::endl;
     std::cout << std::fixed << std::setprecision(2) << "double: " << d << " (detected type)" << std::endl;
     return ;
   }
--- 06 casts/ex00/ScalarConverter.hpp
+++ 06 casts/ex00/ScalarConverter.hpp
@@ -1,24 +1,12 @@
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
-# include <iostream>
-# include <string>
-# include <cstdlib>
+# include <stdlib.h> // strtof atoi strtof strtod
+# include <cfloat>   // FLT_MAX
+# include <climits>  // CHAR_MAX INT_MAX
+# include <iostream> // cout
 # include <cerrno>
-# include <climits>
-# include <cfloat>
-# include <string>
-# include <exception>
-# include <iostream>
-# include <limits>
-# include <stdlib.h>
-# include <iostream>
-# include <iomanip>
-# include <math.h>
-# include <bitset>
-# include <stdio.h>
-
-// too mach headers ?
+# include <iomanip>  // setprecision
 
 class ScalarConverter {
   private:
@@ -30,15 +18,8 @@ class ScalarConverter {
                   ~ScalarConverter ();
   static void     convert(std::string str);
   static bool     fInTheEnd(std::string s);
-  static int      strlen(std::string s);
   static bool     thereIsPoint(std::string s);
-  static bool     isSingleDigitNumber(std::string s);
-  static bool     isChar(std::string s);
-  static bool     isDisplayableChar(std::string s);
-  static bool     isNonDisplayableChar(std::string s);
-  static bool     isDisplayableChar(double d);
-  static bool     isNonDisplayableChar(double d);
-  static bool     isDisplayableChar(char c);
+  static bool     isInteger(std::string s);
 };
 
 #endif
