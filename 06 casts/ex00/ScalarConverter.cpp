57a067c4e6f8c9c1a10be571ef55162ad0299618
ann <internet124816@gmail.com>
2024-01-15 14:06:04 +0100

    06/ex00 char fixed

--- 06 casts/ex00/ScalarConverter.cpp
+++ 06 casts/ex00/ScalarConverter.cpp
@@ -37,9 +37,12 @@ bool ScalarConverter::isSingleDigitNumber(std::string s) {
   return false;
 }
 
-// isdigit authorised ?
 bool ScalarConverter::isChar(std::string s) {
-  return (ScalarConverter::strlen(s) == 1 && s[0] >= CHAR_MIN && !std::isdigit(s[0]));
+  return (ScalarConverter::strlen(s) == 1 && s[0] >= CHAR_MIN && !(s[0] >= '0' && s[0] <= '9'));
+}
+
+bool ScalarConverter::isDisplayableChar(std::string s) {
+  return (ScalarConverter::strlen(s) == 1 && s[0] >= 32 && s[0] <= 126 && !(s[0] >= '0' && s[0] <= '9'));
 }
 
 bool ScalarConverter::isDisplayableChar(double d) {
@@ -50,18 +53,11 @@ bool ScalarConverter::isNonDisplayableChar(double d) {
   return (d >= CHAR_MIN && d <= CHAR_MAX && (d < 32 || d > 126));
 }
 
-bool ScalarConverter::isDisplayableChar(std::string s) {
-  return (ScalarConverter::strlen(s) == 1 && s[0] >= 32 && s[0] <= 126 && !std::isdigit(s[0]));
+bool ScalarConverter::isDisplayableChar(char c) {
+  return (c >= 32 && c <= 126);
 }
 
 bool ScalarConverter::isNonDisplayableChar(std::string s) {
-  std::cout << "strlen(s) = "                      << ScalarConverter::strlen(s) << std::endl; 
-  std::cout << "s[0]      = ["                     << s[0] << "]" << std::endl;
-  printf("s[0] = %d = %c\n", s[0], s[0]);
-  std::cout << s[0] << " >= " << CHAR_MIN << " : " << (s[0] >= CHAR_MIN) << std::endl;
-  std::cout << "2 < 32    : "                      << (2 < 32) << std::endl;
-  std::cout << s[0] << " < 32    : "               << (s[0] < 32) << std::endl;
-  std::cout << s[0] << " > "                       << CHAR_MIN << "  : " << (s[0] > 126) << std::endl;
   return (ScalarConverter::strlen(s) == 1 && s[0] >= CHAR_MIN && (s[0] < 32 || s[0] > 126));
 }
 
@@ -97,8 +93,8 @@ void ScalarConverter::convert(std::string s) {
     else
       std::cout << "char:   Non displayable" << std::endl;
     std::cout << "int:    " << static_cast<int>(c) << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "float:  " << static_cast<float>(c) << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(c) << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "float:  " << static_cast<float>(c) << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "double: " << static_cast<double>(c) << std::endl;
     std::cout << "(detected type: char)" << std::endl;
     return ;
   }
@@ -115,53 +111,51 @@ void ScalarConverter::convert(std::string s) {
   }
   if (d >= INT_MIN && d <= INT_MAX && !ScalarConverter::fInTheEnd(s) && !thereIsPoint(s)) {
     int i = std::atoi(s.c_str());
-    if (ScalarConverter::isDisplayableChar(d))
+    if (static_cast<char>(i) >= 32 && static_cast<char>(i) <= 126)
       std::cout << "char:   '" << static_cast<char>(i) << "'" << std::endl;
-    else if (ScalarConverter::isNonDisplayableChar(d))
-      std::cout << "char:   Non displayable" << std::endl;
     else
-      std::cout << "char:   impossible" << std::endl;
+       std::cout << "char:   Non displayable" << std::endl;
+    // else if (ScalarConverter::isNonDisplayableChar(d) && ScalarConverter::isNonDisplayableChar(s))
+    // else
+    //   std::cout << "char:   string-argument out of range [CHAR_MIN; CHAR_MAX]" << std::endl;
     std::cout << "int:    " << i << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "float:  " << static_cast<float>(i) << "f" << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(i) << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "float:  " << static_cast<float>(i) << "f" << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "double: " << static_cast<double>(i) << std::endl;
     std::cout << "(detected type: int)" << std::endl;
     return ;
   }
   if (d >=-FLT_MAX && d <= FLT_MAX && ScalarConverter::fInTheEnd(s)) {
     float f = std::strtof(s.c_str(), NULL);
-    if (ScalarConverter::isDisplayableChar(d))
+    if (static_cast<char>(f) >= 32 && static_cast<char>(f) <= 126)
       std::cout << "char:   '" << static_cast<char>(f) << "'" << std::endl;
-    else if (ScalarConverter::isNonDisplayableChar(d))
-      std::cout << "char:   Non displayable" << std::endl;
     else
-      std::cout << "char:   impossible" << std::endl;
+      std::cout << "char:   Non displayable" << std::endl;
+    // else if (ScalarConverter::isNonDisplayableChar(d) && ScalarConverter::isNonDisplayableChar(s))
+    // else
+    //   std::cout << "char:   string-argument out of range [CHAR_MIN; CHAR_MAX]" << std::endl;
     if (d >= INT_MIN && d <= INT_MAX)
       std::cout << "int:    " << static_cast<int>(f) << std::endl;
     else
-      std::cout << "int:    impossible" << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "float:  " << f << "f" << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(f) << std::endl;
+      std::cout << "int:    string-argument out of range [INT_MIN; INT_MAX]" << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "float:  " << f << "f" << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "double: " << static_cast<double>(f) << std::endl;
     std::cout << "(detected type: float)" << std::endl;
     return ;
   }
   if (!ScalarConverter::fInTheEnd(s)) {
-    if (ScalarConverter::isDisplayableChar(d))
+    if (static_cast<char>(d) >= 32 && static_cast<char>(d) <= 126)
       std::cout << "char:   '" << static_cast<char>(d) << "'" << std::endl;
-    else if (ScalarConverter::isNonDisplayableChar(d))
-      std::cout << "char:   Non displayable" << std::endl;
     else
-      std::cout << "char:   impossible" << std::endl;
+      std::cout << "char:   Non displayable" << std::endl;
     if (d >= INT_MIN && d <= INT_MAX)
       std::cout << "int:    " << static_cast<int>(d) << std::endl;
     else
-      std::cout << "int:    impossible" << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "d       " << d << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "FLT_MAX " << FLT_MAX << std::endl;
+      std::cout << "int:    string-argument out of range [INT_MIN; INT_MAX]" << std::endl;
     if (d >= -FLT_MAX && d <= FLT_MAX)
-      std::cout << std::fixed << std::setprecision(1) << "float:  " << static_cast<float>(d) << "f" << std::endl;
+      std::cout << std::fixed << std::setprecision(2) << "float:  " << static_cast<float>(d) << "f" << std::endl;
     else
-      std::cout << "float:  impossible" << std::endl;
-    std::cout << std::fixed << std::setprecision(1) << "double: " << d << std::endl;
+      std::cout << "float:  string-argument out of range [FLT_MIN; FLT_MAX]" << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "double: " << d << std::endl;
     std::cout <<  "(detected type: double)" << std::endl;
     return ;
   }
--- 06 casts/ex00/ScalarConverter.hpp
+++ 06 casts/ex00/ScalarConverter.hpp
@@ -17,25 +17,28 @@
 # include <math.h>
 # include <bitset>
 # include <stdio.h>
+
 // too mach headers ?
-// no constructors
-// private constructor!
 
 class ScalarConverter {
-  public:
-    static void convert(std::string str);
+  private:
+                  ScalarConverter  ();
+                  ScalarConverter  (const ScalarConverter &obj);
+  ScalarConverter &operator =      (const ScalarConverter &obj);
 
   public:
-  // utils:
-    static bool fInTheEnd(std::string s);
-    static int  strlen(std::string s);
-    static bool thereIsPoint(std::string s);
-    static bool isSingleDigitNumber(std::string s);
-    static bool isChar(std::string s);
-    static bool isDisplayableChar(std::string s);
-    static bool isNonDisplayableChar(std::string s);
-    static bool isDisplayableChar(double d);
-    static bool isNonDisplayableChar(double d);
+                  ~ScalarConverter ();
+  static void     convert(std::string str);
+  static bool     fInTheEnd(std::string s);
+  static int      strlen(std::string s);
+  static bool     thereIsPoint(std::string s);
+  static bool     isSingleDigitNumber(std::string s);
+  static bool     isChar(std::string s);
+  static bool     isDisplayableChar(std::string s);
+  static bool     isNonDisplayableChar(std::string s);
+  static bool     isDisplayableChar(double d);
+  static bool     isNonDisplayableChar(double d);
+  static bool     isDisplayableChar(char c);
 };
 
 #endif
