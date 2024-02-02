80440562955187b3204e0a3f39751b3db68f5e5c
ak <ak@ak.com>
2024-02-02 23:47:29 +0100

    08, 09 in progress

--- 09 STL/ex01/main.cpp
+++ 09 STL/ex01/main.cpp
@@ -1,7 +1,6 @@
 // at least one container, explain 
-// use c++
-// # $(NAME)
-// The program name is RPN
+// c++
+// # $(NAME), The program name is RPN
 // takes an inverted Polish mathematical expression as arg
 // The numbers used in this operation and passed as arguments will always be less than 10
 // The calculation itself but also the result do not take into account this rule
