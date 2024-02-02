d0a8b06ad2769332e7268d3defd15eaf4e559b8c
ak <ak@ak.com>
2024-02-02 23:23:11 +0100

    08, 09 in progress

--- 09 STL/ex00/main.cpp
+++ 09 STL/ex00/main.cpp
@@ -1,4 +1,4 @@
-// use the standard containers, at least one container
+// at least one container, explain 
 // once a container is used you cannot use it for the rest of the module
 // use c++
 // # $(NAME)
@@ -8,3 +8,15 @@
 // If the date used in the input does not exist in your DB,
 // then use the closest date contained in your DB
 // use the lower date and not the upper one
+
+// use an empty file 
+// a file with errors (a basic example exists in the subject)
+// The prog must not stop its execution 
+// a wrong date
+// enter a value greater than 1000 or less than 0
+
+// use the "input.csv" file located at the top of this page
+// modify this file
+// run the prog with input.csv as parameter 
+// compare some dates manually with the specified value.
+// If the date does not exist in the database, the program will have to use the nearest lower date
