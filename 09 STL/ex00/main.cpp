064ba9c472ecf7fb6c1fd7b871dacc932a659243
ak <ak@ak.com>
2024-02-02 23:11:58 +0100

    08, 09 in progress

--- 09 STL/ex00/main.cpp
+++ 09 STL/ex00/main.cpp
@@ -2,3 +2,9 @@
 // once a container is used you cannot use it for the rest of the module
 // use c++
 // # $(NAME)
+// The program name is btcs
+// handle possible errors with an appropriate error message
+
+// If the date used in the input does not exist in your DB,
+// then use the closest date contained in your DB
+// use the lower date and not the upper one
--- 09 STL/ex01/main.cpp
+++ 09 STL/ex01/main.cpp
@@ -1,3 +1,9 @@
 // use the standard containers, at least one container
 // use c++
 // # $(NAME)
+// The program name is RPN
+// takes an inverted Polish mathematical expression as arg
+// The numbers used in this operation and passed as arguments will always be less than 10
+// The calculation itself but also the result do not take into account this rule
+// handle + - / *
+// don’t need to manage the brackets or decimal numbers
