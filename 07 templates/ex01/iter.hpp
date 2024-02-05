2b8810015f9129eaaa9311bbb502403d4898255f
ann <internet124816@gmail.com>
2024-02-05 20:43:20 +0100

    07/ex01 fixed template <typename T1, typename T2>

--- 07 templates/ex01/iter.hpp
+++ 07 templates/ex01/iter.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 
-template <typename T1, typename T2, typename FUNC_T> void iter(T1* arr, int len, FUNC_T (*func)(T2&)) {
+template <typename T1, typename T2> void iter(T1* arr, int len, void (*func)(T2&)) {
     if (arr == NULL)
         return ;
     for (int i = 0; i < len; i++)
