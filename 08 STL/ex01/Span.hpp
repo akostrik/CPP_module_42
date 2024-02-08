0abe2cf0e6c6b1ddfee533ae4475e642980bc443
akostrik <internet124816@gmail.com>
2024-02-08 04:21:05 +0100

    08/ex01 addManyNumbers in progress

--- 08 STL/ex01/Span.hpp
+++ 08 STL/ex01/Span.hpp
@@ -21,9 +21,18 @@ class Span : public std::vector<unsigned int> {
     unsigned int  shortestSpan();
     unsigned int  longestSpan();
 
-  // prennent une plage d’itérateurs afin d’ajouter une séquence d’éléments au container
-  template <typename T>
-  void addManyNumbers(T cont) {
-    (void)cont;
-  };
+    template <typename Iter>
+    void addManyNumbers(Iter &i, Iter &end) {
+    //void Show(std::list<std::string>::const_iterator bgn, std::list<std::string>::const_iterator end);
+       for (; i != end; ++i)
+          addNumber(3);
+
+      // for (Iter it = begin; it != end; it++) {
+      //   try {
+      //     addNumber(it.value);
+      //   } catch (std::exception &e) {
+      //     std::cout << e.what() << std::endl;
+      //   }
+      // }
+    };
 };
--- 08 STL/ex01/main.cpp
+++ 08 STL/ex01/main.cpp
@@ -4,11 +4,22 @@
 int main() {
   Span s = Span(5);
 
-  s.addNumber(7);
-  s.addNumber(5);
-  s.addNumber(13);
-  s.addNumber(10);
-  s.addNumber(20);
+  // try {
+  //   s.addNumber(7);
+  //   s.addNumber(5);
+  //   s.addNumber(13);
+  //   s.addNumber(10);
+  //   s.addNumber(20);
+  //   s.addNumber(25);
+  // } catch (std::exception &e) {
+  //   std::cout << e.what() << std::endl;
+  // }
+
+  std::vector<unsigned int> v = std::vector<unsigned int>();
+  v.push_back(7);
+  v.push_back(5);
+  v.push_back(10);
+  s.addManyNumbers(v.begin(), v.end());
 
   for (const int& i : s) 
     std::cout << i << "  ";
