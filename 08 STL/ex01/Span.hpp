2de209c47d11c2fc8a21949e9c40e38db911233c
ann <internet124816@gmail.com>
2024-02-09 16:50:55 +0100

    08/ex01 iterator ok

--- 08 STL/ex01/Span.hpp
+++ 08 STL/ex01/Span.hpp
@@ -22,19 +22,20 @@ class Span : public std::vector<unsigned int> {
     unsigned int  longestSpan();
 
     template <typename Iter>
-    void addManyNumbers(Iter &i, Iter &end) {
-    //void Show(std::list<std::string>::const_iterator bgn, std::list<std::string>::const_iterator end);
-      //for (; i != end; ++i) {
+    void addManyNumbers(Iter &begin, Iter &end) {
+      std::cout << "call addNumber(" << begin << ", " << *(&begin+1) << ", " << *(&begin+2) << ")" << std::endl;
+      std::cout << "begin = " << begin << ", &begin = " << &begin << ", &end = " << &end << std::endl;
+      //void Show(std::list<std::string>::const_iterator bgn, std::list<std::string>::const_iterator end);
+      for (Iter *it = &begin; it != &end; ) {
 //        try {
-          std::cout << "call addNumber(" << i << ")" << std::endl;
-          addNumber(i);
-          addNumber(*(&i+1));
-          addNumber(*(&i+2));
-          std::cout << "return " << std::endl;
+          std::cout << "it = " << it << ", *it = " << *it << ", addNumber(" << *it << ")" << std::endl;
+          addNumber(*it);
+          //it = *(&it+1);
+          it++;
         // } catch (std::exception &e) {
         //  std::cout << e.what() << std::endl;
         // }
-      //}
+      }
       (void)end;
     };
 };
