2173b85dd21d5b9c6593c3fb8f60e5381be730c8
ann <internet124816@gmail.com>
2024-02-09 23:51:41 +0100

    08/ex01 qdd mqny numbers with list ok

--- 08 STL/ex01/Span.hpp
+++ 08 STL/ex01/Span.hpp
@@ -3,6 +3,8 @@
 #include <exception>
 #include <climits>
 #include <vector>
+#include <list>
+#include <iterator>
 
 class Span : public std::vector<unsigned int> {
   private:  
@@ -21,16 +23,40 @@ class Span : public std::vector<unsigned int> {
     unsigned int  shortestSpan();
     unsigned int  longestSpan();
 
-    template <typename Iter>
-    void addManyNumbers(Iter &begin, Iter &end) {
-      for (Iter *it = &begin; it != &end; it++) {
-        try {
-          addNumber(*it);
-        } catch (std::exception &e) {
-          std::cout << e.what() << std::endl;
-          break;
-        }
+
+    template <typename T>
+    void addNumber(T begin, T end) {
+      for (; begin != end; begin++) {
+        // if (size == _max_size)
+        //     throw FullException();
+        addNumber(begin);
       }
-      (void)end;
-    };
+    }
+
+    // template <typename Iter>
+    // void addNumber(Iter &begin, Iter &end) {
+    //   std::cout << "     addMany(" << &begin << ", " << &end << ")\n";
+    //   std::cout << "2) list: ";
+    //   for(unsigned int* ittt = &begin; ittt != (&begin + (sizeof begin / sizeof *(&begin))); ittt++) {
+    //     std::cout << *ittt << " ";
+    //   }
+
+    //   std::cout << std::endl << "3) list: ";
+    //   int k = 0;
+    //   //vector<unsigned int>::iterator itt(begin);
+    //   //for (Iter *it = itt; it != &end && k <=5 ; std::advance(it, 1), k++)
+    //   for (Iter *it = &begin; it != &end && k <=2 ; std::advance(it, 1), k++)
+    //     std::cout << *it << " ";
+    //   std::cout << std::endl;
+
+    //   k = 0;
+    //   for (Iter *it = &begin; it != &end && k <= 2; it++, k++) {
+    //     try {
+    //       addNumber(*it);
+    //     } catch (std::exception &e) {
+    //       std::cout << e.what() << std::endl;
+    //       break;
+    //     }
+    //   }
+    // };
 };
--- 08 STL/ex01/main.cpp
+++ 08 STL/ex01/main.cpp
@@ -1,42 +1,58 @@
 #include "Span.hpp"
 #include <iostream>
-#define N 5
+#include <list>
+#define N 6
 
 int main() {
-  Span s                      = Span(N);
-  std::vector<unsigned int> v = std::vector<unsigned int>();
+  Span s                   = Span(N);
+  std::vector<unsigned int> v{1,2,7};
+  std::list<unsigned int>   l{4,88,6};
 
+  // try {
+  //   s.addNumber(7);
+  // } catch (std::exception &e) {
+  //   std::cout << e.what() << std::endl;
+  // }
+  // std::cout << std::endl;
+
+  std::cout << "1) v.begin() = " << *v.begin() << std::endl;
   try {
-    s.addNumber(7);
+    s.addNumber(*v.begin(), *v.end());
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+  std::cout << std::endl;
 
-  v.push_back(5);
-  v.push_back(10);
-  v.push_back(2);
-  v.push_back(15);
-  v.push_back(20);
+
+  /////////////////////////////////////////////////////////////
+  std::cout << "1) l.begin()     = " << *l.begin() << std::endl;
+  std::cout << "1) list: ";
+  for (std::list<unsigned int>::iterator it = l.begin() ; it != l.end(); it++)
+    std::cout << *it << " ";
+  std::cout << std::endl << std::endl;
+
   try {
-    s.addManyNumbers(*v.begin(), *v.end());
+    std::cout << "call addMany(" << *l.begin() << ", " << *l.end() << ")\n";
+    s.addNumber(*l.begin(), *l.end());
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+  std::cout << std::endl;
 
   std::cout << std::endl << "not sorted : ";
-  for (const unsigned int& i : s) 
+  for (const unsigned int& i : s) // range  C++11
     std::cout << i << "  ";
 
-  try {
-    std::cout << std::endl << s.shortestSpan() << " (shortestSpan)" << std::endl;
-  } catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
-  }
-  try {
-    std::cout << std::endl << s.longestSpan()  << " (longuestSpan)" << std::endl;
-  } catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
-  }
+  // try {
+  //   std::cout << std::endl << s.shortestSpan() << " (shortestSpan)" << std::endl;
+  // } catch (std::exception &e) {
+  //   std::cout << e.what() << std::endl;
+  // }
+  // try {
+  //   std::cout << std::endl << s.longestSpan()  << " (longuestSpan)" << std::endl;
+  // } catch (std::exception &e) {
+  //   std::cout << e.what() << std::endl;
+  // }
 
   return 0;
 }
