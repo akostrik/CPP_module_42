94b8bf28f2c9a8d59e5231df07c12dca13e49c6f
akostrik <internet124816@gmail.com>
2024-02-10 21:47:11 +0100

    08 verifications and tests

--- 08 STL/ex02/MutantStack.hpp
+++ 08 STL/ex02/MutantStack.hpp
@@ -9,58 +9,49 @@
 #include <stack>
 #include <iterator>
 
+// c = the underlying container
+
+// template <class T, class Container=std::deque <T> >
+// class MutantStack: public std::stack<T, Container> {
+
 template <typename T>
 class MutantStack : public std::stack<T> {
   public:
 
-// to give to stack the access to its underlying container (container_type)
+  typedef typename std::stack<T>::container_type::iterator iterator; // so stack can access its underlying container (container_type)
 
-  typedef typename std::stack<T>::container_type::iterator iterator;
+               ~MutantStack()                     {}
+               MutantStack ()                       : std::stack<T>()  {}
+  //             MutantStack (const MutantStack<T> &o): std::stack<T>(o) {} uselesss
+  //MutantStack& operator=   (const MutantStack& o) { this->c = o.c; return *this; }; useless ?
+  //T*           operator++  ()                     { (*this)++;     return *this; }; useless
 
-               MutantStack () {}
-               ~MutantStack() {}
-
-  MutantStack(const MutantStack<T>  &o): std::stack<T>(o) {} 
- 
-  MutantStack& operator= (const MutantStack& o) { 
-    this->c = o.c;
-    //std::stack<T>::operator=(o); 
-    return *this; 
-  };
-
-
-
-  T*           operator->  ()                     { return *this; };
-  T&           operator*   () const               { return &this; };
-  T*           operator++  ()                     { (*this)++;  return *this; };
-
-  // c = the underlying container
-  iterator     begin       ()                     { return (this->c.begin());  };
-  iterator     end         ()                     { return (this->c.end());    };
-  iterator     rend        ()                     { return (this->c.rend());   };
-  iterator     rbegin      ()                     { return (this->c.rbegin()); };
-  iterator     cbegin      () const               { return (this->c.begin());  };
-  iterator     cend        () const               { return (this->c.end());    };
-  iterator     crbegin     () const               { return (this->c.rbegin()); };
-  iterator     crend       () const               { return (this->c.rend());   };
+  iterator     begin       ()                     { return this->c.begin();  };
+  iterator     end         ()                     { return this->c.end();    };
+  iterator     rend        ()                     { return this->c.rend();   };
+  iterator     rbegin      ()                     { return this->c.rbegin(); };
+  iterator     cbegin      () const               { return this->c.begin();  };
+  iterator     cend        () const               { return this->c.end();    };
+  iterator     crbegin     () const               { return this->c.rbegin(); };
+  iterator     crend       () const               { return this->c.rend();   };
   //             iterator    (T* o)               { this(o); }
 };
 
-template <typename T>
-typename std::stack<T>::container_type::iterator operator++(T o) {
-  typename std::stack<T>::container_type::iterator tmp = *o; 
-  ++(*o);
-  return tmp;
-}
+// template <typename T> useless
+// typename std::stack<T>::container_type::iterator operator++(T o) {
+//   typename std::stack<T>::container_type::iterator tmp = *o; 
+//   ++(*o);
+//   return tmp;
+// }
 
-template <typename T>
-bool operator!=(const typename std::stack<T>::container_type::iterator& a, const typename std::stack<T>::container_type::iterator& b) {
-  return a != b;
-};
+// template <typename T>
+// bool operator!=(const typename std::stack<T>::container_type::iterator& a, const typename std::stack<T>::container_type::iterator& b) {
+//   return a != b;
+// };
 
-template <typename T>
-bool operator==(const typename std::stack<T>::container_type::iterator& a, const typename std::stack<T>::container_type::iterator& b) {
-  return a == b;
-};
+// template <typename T>
+// bool operator==(const typename std::stack<T>::container_type::iterator& a, const typename std::stack<T>::container_type::iterator& b) {
+//   return a == b;
+// };
 
 #endif
--- 08 STL/ex02/main.cpp
+++ 08 STL/ex02/main.cpp
@@ -1,5 +1,4 @@
 // executez en remplaçant la MutantStack par std::list
-// testes !
 #include "MutantStack.hpp"
 #include <list>
 
@@ -7,15 +6,14 @@ int main() {
   MutantStack<int> s;
 
   std::cout << "*** TEST push pop top size" << std::endl;
-  s.push(5);
-  s.push(7);
+  s.push(1);
+  s.push(2);
   std::cout << "top = " << s.top() << ", stze = " << s.size() << std::endl;
   s.pop();
   std::cout << "top = " << s.top() << ", stze = " << s.size() << std::endl;
   s.push(3);
-  s.push(5);
-  s.push(8);
   s.push(4);
+  s.push(5);
   for (MutantStack<int>::iterator it = s.begin(); it != s.end(); ++it)
     std::cout << *it << " ";
   std::cout << std::endl;
@@ -35,7 +33,22 @@ int main() {
     std::cout << *it << " ";
   std::cout << std::endl;
 
-  // std::stack<int> s4(s);
+  std::cout << "*** TEST list" << std::endl;
+  std::list<int> l;
+  l.push_front(1);
+  l.push_front(2);
+  std::cout << "top = " << l.front() << ", stze = " << l.size() << std::endl;
+  l.pop_front();
+  std::cout << "top = " << l.front() << ", stze = " << l.size() << std::endl;
+  l.push_front(3);
+  l.push_front(4);
+  l.push_front(5);
+  std::list<int>::iterator it = l.end();
+  it--;
+  for (; it != l.begin(); it--) 
+    std::cout << *it << " ";
+  std::cout << *it << std::endl;
+  std::cout << "top = " << l.front() << ", stze = " << l.size() << std::endl;
 
   return 0;
 }
