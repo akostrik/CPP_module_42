6c7a488bb41533cef806787a905a1e79fb140867
ann <internet124816@gmail.com>
2024-02-16 06:19:46 +0100

    trim ok

--- 09 STL/ex00/BitcoinExchange.cpp
+++ 09 STL/ex00/BitcoinExchange.cpp
@@ -1,71 +1,30 @@
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange() : std::map<std::string, unsigned long long>() {
-// modify "input.csv"
-	std::ifstream      in(DB_FILE);
-	std::string        line;
-  std::string        date;
-  std::string        dollars;
-  std::string        cents;
-  unsigned long long day_price_in_cents;
-
-	if (!in.is_open())
-		throw std::exception(); // !
-  std::getline(in, line); // skip first line
- 	while (getline (in, line)) {
-    date    = line.substr(0, line.find(","));
-    date.erase(date.find_last_not_of(" \n\r\t") + 1);
-    dollars = line.substr(11, line.find(".") - line.find(",") - 1);
-    dollars.erase(dollars.find_last_not_of(" \n\r\t") + 1);
-    cents   = line.substr(line.find(".") + 1, line.size() - line.find("."));
-    cents.erase(cents.find_last_not_of(" \n\r\t")+1);
-    day_price_in_cents   = 100 * std::strtol(dollars.c_str(), NULL, 10);
-    if (line.find(".") != std::string::npos && cents.size() == 2)
-      day_price_in_cents += std::strtol(cents.c_str(), NULL, 10);
-    else if (line.find(".") != std::string::npos && cents.size() == 1)
-      day_price_in_cents += 10 * std::strtol(cents.c_str(), NULL, 10);
-    this->insert(std::pair<std::string, unsigned long long>(date, day_price_in_cents));
-  }
-	in.close();
-  if (this->size() == 0)
-		throw std::exception(); // file es empty
+void trimR(std::string& s) {
+  std::string::size_type pos = s.find_last_not_of(" \f\n\r\t\v");
+  s.erase(pos + 1);
 }
 
-// BitcoinExchange::BitcoinExchange(const BitcoinExchange& o) : std::map<std::string, unsigned long long>() {
-//   *this = o;
-// }
+void trimL( std::string& s) {
+  std::string::size_type pos = s.find_first_not_of(" \f\n\r\t\v");
+  s.erase(0, pos);
+}
 
-BitcoinExchange::~BitcoinExchange() {}
+void trim(std::string& s) {
+  trimR(s);
+  trimL(s);
+}
 
-// BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& o) {
-//   std::copy(o.begin(), o.end(), this->begin());
-//   return *this;
-// }
+void print_without_trailing_zeros(std::string date, unsigned long long n) {
+  if (n % 100 == 0)
+    std::cout << date << " => " << n << " = " << n / 100 << std::endl;
+  if (n % 10 == 0)
+    std::cout << date << " => " << n << " = " << n / 100 << "." << (n / 10) << std::endl;
+  else
+    std::cout << date << " => " << n << " = " << n / 100 << "." << n % 100 << std::endl;
+}
 
-////////////////////////////////////////////
-// handle possible errors with an appropriate error message
-// use an empty file
-// a file with errors (a basic example exists in the subject)
-// a wrong date
-// run the prog with input.csv as parameter 
-// if the date not exist in your DB, use the closest date contained in your DB, the lower date, not the upper one
-
-// std::string BitcoinExchange::convert(double v) {
-//   std::stringstream ss;                                         // Print v to a string
-//   ss << std::fixed << std::setprecision(2) << v;
-//   std::string str = ss.str();
-//   // Ensure that there is a decimal point somewhere (there should be)
-//   if(str.find('.') != std::string::npos) {
-//     // Remove trailing zeroes
-//     str = str.substr(0, str.find_last_not_of('0')+1);
-//     // If the decimal point is now the last character, remove that as well
-//     if(str.find('.') == str.size()-1)
-//         str = str.substr(0, str.size()-1);
-//   }
-//   return str;
-// }
-
-bool BitcoinExchange::is_valid_date(std::string date) {
+bool is_valid_date(std::string date) {
   regex_t regex;
 
   regcomp(&regex, "[2-9][0-9][0-9][0-9][-]0[13578][-][0-2][0-9]", 0); // compile reg expression // janv march may july aug
@@ -114,14 +73,56 @@ bool BitcoinExchange::is_valid_date(std::string date) {
   return false;
 }
 
-void BitcoinExchange::print_without_trailing_zeros(std::string date, unsigned long long n) {
-  if (n % 100 == 0)
-    std::cout << date << " => " << n << " = " << n / 100 << std::endl;
-  if (n % 10 == 0)
-    std::cout << date << " => " << n << " = " << n / 100 << "." << (n / 10) << std::endl;
-  else
-    std::cout << date << " => " << n << " = " << n / 100 << "." << n % 100 << std::endl;
+///////////////////////////////////////////////////////////////////////////////////
+BitcoinExchange::BitcoinExchange() : std::map<std::string, unsigned long long>() {
+// modify "input.csv"
+	std::ifstream      in(DB_FILE);
+	std::string        line;
+  std::string        date;
+  std::string        dollars;
+  std::string        cents;
+  unsigned long long day_price_in_cents;
+
+	if (!in.is_open())
+		throw std::exception(); // !
+  std::getline(in, line); // skip first line
+ 	while (getline (in, line)) {
+    date    = line.substr(0, line.find(","));
+    //date.erase(date.find_last_not_of(" \n\r\t") + 1);
+    dollars = line.substr(11, line.find(".") - line.find(",") - 1);
+    //dollars.erase(dollars.find_last_not_of(" \n\r\t") + 1);
+    cents   = line.substr(line.find(".") + 1, line.size() - line.find("."));
+    cents.erase(cents.find_last_not_of(" \n\r\t")+1);
+    day_price_in_cents   = 100 * std::strtol(dollars.c_str(), NULL, 10);
+    if (line.find(".") != std::string::npos && cents.size() == 2)
+      day_price_in_cents += std::strtol(cents.c_str(), NULL, 10);
+    else if (line.find(".") != std::string::npos && cents.size() == 1)
+      day_price_in_cents += 10 * std::strtol(cents.c_str(), NULL, 10);
+    this->insert(std::pair<std::string, unsigned long long>(date, day_price_in_cents));
+  }
+	in.close();
+  if (this->size() == 0)
+		throw std::exception(); // file es empty
 }
+
+// BitcoinExchange::BitcoinExchange(const BitcoinExchange& o) : std::map<std::string, unsigned long long>() {
+//   *this = o;
+// }
+
+BitcoinExchange::~BitcoinExchange() {}
+
+// BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& o) {
+//   std::copy(o.begin(), o.end(), this->begin());
+//   return *this;
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// handle possible errors with an appropriate error message
+// use an empty file
+// a file with errors (a basic example exists in the subject)
+// run the prog with input.csv as parameter 
+// if the date not exist in your DB, use the closest date contained in your DB, the lower date, not the upper one
 
 void BitcoinExchange::run(std::string filename) {
 	std::ifstream in(filename.c_str());
@@ -136,7 +137,9 @@ void BitcoinExchange::run(std::string filename) {
   std::getline(in, line);
  	while (getline (in, line)) {
     date      = line.substr(0, line.find("|") - 1);
-    date.erase(date.find_last_not_of(" \n\r\t") + 1);
+    //date.erase(date.find_last_not_of(" \n\r\t") + 1);
+    trim(date);
+    std::cout << "*** date = [" << date << "]" << std::endl;
     value_str = line.substr(line.find("|") + 1, line.size() - line.find("|"));
     value_str.erase(value_str.find_last_not_of(" \n\r\t") + 1);
     value     = std::strtod(value_str.c_str(), NULL);
@@ -157,6 +160,7 @@ void BitcoinExchange::run(std::string filename) {
         day_price_in_cents = (--(this->upper_bound(date)))->second;
       print_without_trailing_zeros(date, (unsigned long long)value * day_price_in_cents);
     }
+    // delete date;
   }
 	in.close();
 }
--- 09 STL/ex00/BitcoinExchange.hpp
+++ 09 STL/ex00/BitcoinExchange.hpp
@@ -8,14 +8,7 @@
 # include <iomanip>
 # include <stdexcept>
 # include <limits>
-// # include <regex> c++11
 # include <regex.h>
-
-// # include <algorithm>
-// # include <exception>
-// # include <iterator>
-// # include <functional>
-// # include <numeric>
 
 # define DB_FILE "data.csv"
 
@@ -26,8 +19,6 @@ class BitcoinExchange : std::map<std::string, unsigned long long> {
 								   ~BitcoinExchange       ();
 	BitcoinExchange& operator=   (const BitcoinExchange& o);
   void             run(std::string filename);
-  bool             is_valid_date(std::string date);
-  void             print_without_trailing_zeros(std::string date, unsigned long long n);
 };
 
 #endif
