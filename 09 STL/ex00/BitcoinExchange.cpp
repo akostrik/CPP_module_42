c8885bde64b14c767447d2ebff5c988d539e191e
akostrik <internet124816@gmail.com>
2024-02-17 06:03:39 +0100

    09/ex00 final

--- 09 STL/ex00/BitcoinExchange.cpp
+++ 09 STL/ex00/BitcoinExchange.cpp
@@ -14,33 +14,30 @@ bool is_valid_date(std::string date) {
   regex_t regex;
   bool ok;
 
-  regcomp(&regex, "[2-9][0-9][0-9][0-9][-](01|03|05|07|08|10|12)[-]([0-2][0-9]|30|31)", REG_EXTENDED); // 31
+  regcomp(&regex, "[0-9][0-9][0-9][0-9][-](01|03|05|07|08|10|12)[-]([0-2][0-9]|30|31)", REG_EXTENDED); // 31
   ok = !regexec(&regex, date.c_str(), 0, NULL, 0);
   regfree(&regex);
   if (ok)
     return true;
 
-  regcomp(&regex, "[2-9][0-9][0-9][0-9][-](02|04|06|09|11)[-]([0-2][0-9]|30)", REG_EXTENDED); // 30
+  regcomp(&regex, "[0-9][0-9][0-9][0-9][-](04|06|09|11)[-]([0-2][0-9]|30)", REG_EXTENDED); // 30
   ok = !regexec(&regex, date.c_str(), 0, NULL, 0);
   regfree(&regex);
   if (ok)
     return true;
 
-  regcomp(&regex, "[2-9][0-9][0-9][0-9][-]02[-]([01][0-9]|2[0-8])", REG_EXTENDED); // 28
+  regcomp(&regex, "[0-9][0-9][0-9][0-9][-]02[-]([01][0-9]|2[0-8])", REG_EXTENDED); // 28
   ok = !regexec(&regex, date.c_str(), 0, NULL, 0);
   regfree(&regex);
   if (ok)
     return true;
 
-  std::string year_str = date.substr(0, date.find("-")); // 29
-  int year = std::strtol(year_str.c_str(), NULL, 10);
-  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-    regcomp(&regex, "[2-9][0-9][0-9][0-9][-]02[-]29", 0);
-    ok = !regexec(&regex, date.c_str(), 0, NULL, 0);
-    regfree(&regex);
-    if (ok)
-      return true;
-  }
+  regcomp(&regex, "[0-9][0-9][0-9][0-9][-]02[-]29", REG_EXTENDED); // 29
+  ok = !regexec(&regex, date.c_str(), 0, NULL, 0);
+  regfree(&regex);
+  int year = std::strtol(date.substr(0, date.find("-")).c_str(), NULL, 10);
+  if (ok && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
+    return true;
 
   return false;
 }
@@ -64,27 +61,26 @@ bool is_valid_db_line(std::string line) {
   return true;
 }
 
-bool is_valid_arg_file_line(std::string line) {
+bool is_valid_arg_file_line(std::string line, double value, std::string firstDate) {
   regex_t     regex;
   bool        ok;
-  std::string date = line.substr(0, line.find(","));
+  std::string date = line.substr(0, line.find("|"));
 
-  regcomp(&regex, "[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9],([0-9]+[.]?[0-9]*|[0-9]*[.][0-9][0-9]?)$", REG_EXTENDED);
+  regcomp(&regex, "[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9][|][-]?([0-9]+[.]?[0-9]*|[0-9]*[.][0-9][0-9]?)$", REG_EXTENDED);
   ok = !regexec(&regex, line.c_str(), 0, NULL, 0);
   regfree(&regex);
-  if (ok && is_valid_date(date))
-    return true;
-  // regcomp(&regex, "[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9],[0-9]*[.][0-9][0-9]?$", REG_EXTENDED);
-  // ok = !regexec(&regex, line.c_str(), 0, NULL, 0);
-  // regfree(&regex);
-  // if (ok && is_valid_date(date))
-  //   return true;
-  if (!is_valid_date(date)) {
-    std::cout << "Error: invalide date " << date << " is the database.\n";
-    return false;
-  }
-  std::cout << "Error: " << line << " : invalid line in the database\n";
-  return false;
+
+  if (!ok)
+    return (std::cout << "Error: " << line << " : invalid line in the argument file.\n", false);
+  if (std::count(line.begin(), line.end(), '-') == 3)
+    return (std::cout << "Error: not a positive number.\n", false);
+  if (value > 10000)
+    return (std::cout << "Error: " << value << " is a too large number.\n", false);
+  if(date < firstDate)
+    return (std::cout << "Error: the date " << date << " is invalid (the earliest possible date is " << firstDate << ").\n", false);
+  if (!is_valid_date(date))
+    return (std::cout << "Error: invalide date " << date << " in the argument file.\n", false);
+  return true;
 }
 
 // modify "input.csv" !!!!!!!!!!!!! empty line!
@@ -117,7 +113,7 @@ BitcoinExchange::BitcoinExchange() : std::map<std::string, unsigned long long>()
       day_price_in_cents += 10 * std::strtol(cents.c_str(), NULL, 10);
     this->insert(std::pair<std::string, unsigned long long>(date, day_price_in_cents));
   }
-	in.close();
+  in.close();
   if (file_is_empty)
     throw std::underflow_error("Error: database file es empty");
 }
@@ -136,43 +132,30 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& o) {
 
 ///////////////////////////////////////////////////////////////////////////////////
 void BitcoinExchange::run(std::string filename) {
-  std::ifstream      in(filename.c_str()); (void)in;
-  // std::string        line;
-  // std::string        date;
-  // std::string        value_str;
-  // double             value;
-  // unsigned long long day_price_in_cents;
-  // bool               file_is_empty = true;
+  std::ifstream      in(filename.c_str());
+  std::string        line;
+  std::string        date;
+  double             value;
+  unsigned long long day_price_in_cents;
+  bool               file_is_empty = true;
 
-  // if (!in.is_open())
-  //   throw std::runtime_error("argument file problem");
-  // std::getline(in, line);
-  // while (getline (in, line)) {
-  //   file_is_empty = false;
-  //   line.erase(std::remove_if(line.begin(),line.end(),isspace),line.end());
-  //   date      = line.substr(0, line.find("|") - 1);
-  //   value_str = line.substr(line.find("|") + 1, line.size() - line.find("|"));
-  //   value     = std::strtod(value_str.c_str(), NULL);
-  //   if (line.find("|") == std::string::npos) 
-  //     std::cout << "Error: bad input => " << line << std::endl;
-  //   else if (value < 0) 
-  //     std::cout << "Error: " << value << " is not a positive number.\n";
-  //   else if (value > 10000) 
-  //     std::cout << "Error: " << value << " is a too large number.\n";
-  //   else if(this->find(date) == this->end() && this->upper_bound(date) == this->begin())
-  //     std::cout << "Error: the date " << date << " is invalid (the earliest possible date is 2009-01-02).\n";
-  //   else if (!is_valid_date(date))
-  //     std::cout << "Error: the date " << date << " is invalid.\n";
-  //   else {
-  //     if (this->find(date) != this->end())
-  //       day_price_in_cents = this->find(date)->second;
-  //     else
-  //       day_price_in_cents = (--(this->upper_bound(date)))->second;
-  //     (void)day_price_in_cents;
-  //     print_without_trailing_zeros(date, (unsigned long long)value * day_price_in_cents);
-  //   }
-  // }
-  // in.close();
-  // if (file_is_empty)
-  //   throw std::underflow_error("Error: argument file es empty");
+  if (!in.is_open())
+    throw std::runtime_error("argument file problem");
+  std::getline(in, line);
+  while (getline (in, line)) {
+    file_is_empty = false;
+    line.erase(std::remove_if(line.begin(),line.end(),isspace),line.end());
+    date = line.substr(0, line.find("|"));
+    value = std::strtod(line.substr(line.find("|") + 1, line.size() - line.find("|")).c_str(), NULL);
+    if (!is_valid_arg_file_line(line, value, this->begin()->first))
+      continue ;
+    if (this->find(date) != this->end())
+      day_price_in_cents = this->find(date)->second;
+    else
+      day_price_in_cents = (--(this->upper_bound(date)))->second;
+    print_without_trailing_zeros(date, (unsigned long long)value * day_price_in_cents);
+  }
+  in.close();
+  if (file_is_empty)
+    throw std::underflow_error("Error: argument file es empty");
 }
