2bf369c754f4535cb181e77685fb51b023f8cbfa
akostrik <internet124816@gmail.com>
2024-02-17 20:35:04 +0100

    09/ex01 finished

--- 09 STL/ex01/RPN.cpp
+++ 09 STL/ex01/RPN.cpp
@@ -8,27 +8,7 @@ RPN::RPN(const RPN& o) : std::stack<float>() {
   *this = o;
 }
 
-template <class ADAPTER>
-typename ADAPTER::container_type & get_container (ADAPTER &a) {
-  struct hack : ADAPTER {
-    static typename ADAPTER::container_type & get (ADAPTER &a) {
-      return a.*&hack::c;
-    }
-  };
-  return hack::get(a);
-}
-
-// template <class T, class S, class C>
-//   S& Container(std::priority_queue<T, S, C>& q) {
-//     struct HackedQueue : private std::priority_queue<T, S, C> {
-//       static S& Container(std::priority_queue<T, S, C>& q) {
-//         return q.*&HackedQueue::c;
-//       }
-//     };
-//   return HackedQueue::Container(q);
-// }
-
-RPN& RPN::operator=(const RPN& o) { // to test
+RPN& RPN::operator=(const RPN& o) {
   std::stack<float>::container_type underliyng_cont_o    = o.c;
   this->c = underliyng_cont_o;
   return *this;
