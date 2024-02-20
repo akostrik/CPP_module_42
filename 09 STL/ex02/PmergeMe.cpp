acd627979b6940fba78ccae5416fb952323762ef
ann <internet124816@gmail.com>
2024-02-20 05:15:17 +0100

    09/02 binary search in progress

--- 09 STL/ex02/PmergeMe.cpp
+++ 09 STL/ex02/PmergeMe.cpp
@@ -13,24 +13,6 @@ void PmergeMe::calc_order_insertions(int argc) {
     for(int v = where_to_stop; i <= where_to_stop; v--, i++)
       _order[i] = v;
   }
-  std::cout << std::endl << "order: ";
-  for(int i = 0; i < nb_insertions; i++)
-    std::cout << _order[i] << " ";
-}
-
-list_iterator calc_where_insert(list_iterator begin, list_iterator end, unsigned int a) {
-  while(1) {
-    list_iterator middle = begin;
-    std::advance(middle, std::distance(begin, end) / 2);
-    if(a == *middle)
-      return middle ;
-    else if(a < *middle)
-      end = middle;
-    else if(a > *middle)
-      begin = middle;
-    if(std::distance(begin, end) <= 1)
-      return begin;
-  }
 }
 
 std::map<unsigned int, unsigned int> put_list_to_map(std::list<unsigned int> lst) {
@@ -41,14 +23,14 @@ std::map<unsigned int, unsigned int> put_list_to_map(std::list<unsigned int> lst
     map.insert(std::pair<unsigned int, unsigned int>(*it1, *it2));
     ++it1;
     ++it2;
-    if (it2 == lst.end()) // even number of elements 1 2 3 4* .*
+    if (it2 == lst.end()) // for ex 1 2 3 4* .*
       break ;
     ++it1;
     ++it2;
-    if (it2 == lst.end()) // odd  number of elements 1 2 3 4 5* .*
+    if (it2 == lst.end()) // for ex 1 2 3 4 5* .*
       break ;
   }
-  if(std::distance(lst.begin(), lst.end()) % 2 == 1 )
+  if(std::distance(lst.begin(), lst.end()) % 2 == 1) // for the last map elt (5,0)
     map.insert(std::pair<unsigned int, unsigned int>(*it1, 0));
   return map;
 }
@@ -61,38 +43,19 @@ std::list<unsigned int> put_map_keys_to_list(std::map<unsigned int, unsigned int
 }
 
 unsigned int *PmergeMe::put_map_values_to_array_in_order(std::map<unsigned int, unsigned int> map) {
-  size_t size_array;
   size_t i;
   map_iterator it;
 
-  unsigned int last_value = (--(map.end()))->second;
-  size_array = map.size();
-  if (last_value == 0)
+  size_t size_array = map.size();
+  if ((--(map.end()))->second == 0)
     size_array--;
-  std::cout << std::endl << "size array = " << size_array << std::endl;
   unsigned int *arr = new unsigned int[size_array];
-  for(it = map.begin(), i = 0; it != map.end() && it->second != 0; ++it, ++i) {
+  for(it = map.begin(), i = 0; it != map.end() && it->second != 0; ++it, ++i)
     arr[_order[i]] = it->second;
-    std::cout << "i = " << i << ", insert = " << it->second << " to the place " << _order[i] << std::endl;
-  }
-  std::cout << std::endl << "arr: ";
-  for(size_t i =0; i < size_array; i++)
-    std::cout << arr[i] << " ";
   return arr;
 }
 
-void erase_every_2nd_elt(std::list<unsigned int> *lst) {
-  size_t size = lst->size();
-  std::list<unsigned int>::iterator it = lst->begin();
-  while(size--) {
-    std::list<unsigned int>::iterator toErase = it;
-    it++;
-    if (size%2 == 1)
-      lst->erase(toErase);
-  }
-}
-
-void sort_every_pair(std::map<unsigned int, unsigned int> *map) {
+void sort_2_elts_of_pair(std::map<unsigned int, unsigned int> *map) {
   for(map_iterator it = map->begin(); it != map->end();) {
     if(it->first < it->second) {
       map_iterator to_erase = it;
@@ -105,13 +68,36 @@ void sort_every_pair(std::map<unsigned int, unsigned int> *map) {
   }
 }
 
+void binary_search_insert(std::list<unsigned int> lst, list_iterator end, unsigned int a) {
+  // list_iterator begin = lst.begin();
+  // list_iterator insert_after_this_elt;
+  // int k = 0;
+  (void)a;
+  std::cout << "binary_search_insert " << a << " between " << *(lst.begin()) << " and " << *end << std::endl;
+  // while(k++ < 5) {
+  //   list_iterator middle = begin;
+  //   std::advance(middle, std::distance(begin, end) / 2); // left ?
+  //   if(a == *middle) {
+  //     insert_after_this_elt = middle ;
+  //     break ;
+  //   }
+  //   else if(a < *middle)
+  //     end = middle;
+  //   else if(a > *middle)
+  //     begin = middle;
+  //   if(std::distance(lst.begin(), end) <= 1) {
+  //     insert_after_this_elt = begin;
+  //     break ;
+  //   }
+  // }
+  //lst.insert(insert_after_this_elt, a);
+}
+
 /////////////////////////////////////////////////////////////////////////////// CONSTRUCTORS
+
 PmergeMe::PmergeMe() {}
 
-PmergeMe::~PmergeMe() {
-  delete _order;
-  // delete arr
-}
+PmergeMe::~PmergeMe() { delete _order; } //
 
 PmergeMe::PmergeMe(const PmergeMe& o) { *this = o; }
 
@@ -133,7 +119,7 @@ void PmergeMe::run(std::list<unsigned int> *lst) {
     return ;
 
   std::map<unsigned int, unsigned int> map = put_list_to_map(*lst); // [2 1] [4 3] [6 5] [8 7] [9 0]
-  sort_every_pair(&map);
+  sort_2_elts_of_pair(&map);
   std::cout << std::endl << "map with sorted pairs: ";
   for (map_iterator it = map.begin(); it != map.end(); it++)
     std::cout << "[" << it->first << "," << it->second << "] ";
@@ -148,14 +134,23 @@ void PmergeMe::run(std::list<unsigned int> *lst) {
   std::cout << std::endl << "arr      : ";
   for (size_t i = 0; i < size_array; i++)
     std::cout << arr[i] << " ";
+  std::cout << std::endl;
 
   // lalf_list sort recursively
 
-  // int i = 0;
-  // for (list_iterator it = ++(++(lst.begin())); it != list.end() && *it != 0; ++it, i++)
-  //   my_insert(half_list.begin(), , arr[i]);
+  list_iterator it_end = ++(half_lst.begin());
+  for (size_t i = 0; i < size_array; i++) {
+    if (arr[i] < *(half_lst.begin())) {
+      std::cout << "insert " << arr[i] << " in the beginning\n";
+      half_lst.push_front(arr[i]);
+    }
+    else
+      binary_search_insert(half_lst, it_end, arr[i]);
+    ++it_end;
+  }
 
   std::cout << std::endl;
+  delete arr;
 }
 
 void PmergeMe::run() {
